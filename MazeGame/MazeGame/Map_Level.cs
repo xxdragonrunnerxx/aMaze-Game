7b6604887ece4b11fa46f41e539d5ea20093baf8
Bradley Massey <bjmassey@mix.wvu.edu>
2018-03-30 08:56:30 -0400

    setup

---
+++ MazeGame/MazeGame/Map_Level.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGame
+{
+    class Map_Level
+    {
+        int[] location = new int[3];
+        int[] currentLocation = new int[3];
+
+    }
+}
