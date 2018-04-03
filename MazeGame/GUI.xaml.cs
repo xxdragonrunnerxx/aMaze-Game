18b1ea680fe0310ea8219fee0445948e54f016b2
Bradley Massey <bjmassey@mix.wvu.edu>
2018-04-03 19:12:53 -0400

    setup Gui
    Setup Gui builder

---
+++ MazeGame/GUI.xaml.cs
@@ -0,0 +1,46 @@
+﻿/***********************************************************
+  * Bradley Massey
+  * Daniel Reiter
+  * Erika Goad
+  * Galina Vitvitskaya
+  * 4/1/2018
+  * 
+  * Program: MazeGame
+  * 
+  * Class: Game
+  * 
+  * Variables:
+  * 
+  * Methods:
+  * Main()
+  ***********************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
